462d4b32a6dab7af62a26837253b7afdf5db20c0
myphotosecond2-dev <myphotosecond2@gmail.com>
2026-04-17 17:41:34 +0530

    fix auth route esm export issue

--- routes/auth.js
+++ routes/auth.js
@@ -11,16 +11,10 @@ import {
 
 import { protect } from "../middleware/authMiddleware.js";
 
-// Send OTP
+// routes
 router.post("/send-otp", sendOTPHandler);
-
-// Verify OTP
 router.post("/verify-otp", verifyOTPHandler);
-
-// Register user
 router.post("/register", registerHandler);
-
-// Get current user
 router.get("/me", protect, getMe);
 
 export default router;
